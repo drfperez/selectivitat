9f4a4e4f1f8375779ce83233979347df104d7a3b
Francisco Pérez <drfperez@gmail.com>
2023-03-27 11:13:59 +0200

    Create problema2.py

--- problema2.py
+++ problema2.py
@@ -22,7 +22,6 @@ def calcular():
     # Mostrar el resultat
     resultado_label.config(text="La potència elèctrica útil del parc eòlic és de {:.2f} kW.".format(potencia_electrica))
     beneficio_label.config(text="El benefici anual del parc eòlic és de {:.2f} euros.".format((potencia_electrica * precio_venta * 365) / 100))
-    
 
 # Crear la finestra
 window = Tk()
@@ -45,10 +44,38 @@ densidad_aire_label = Label(window, text="Densitat de l'aire en g/m³:")
 densidad_aire_entry = Entry(window)
 coef_aprovechamiento_viento_label = Label(window, text="Coeficient d'aprofitament del vent:")
 coef_aprovechamiento_viento_entry = Entry(window)
+precio_venta_label = Label(window, text="Preu de venda de l'energia elèctrica en €/kWh:")
+precio_venta_entry = Entry(window)
+
+calcular_button = Button(window, text="Calcular", command=calcular)
+
+resultado_label = Label(window, text="")
+beneficio_label = Label(window, text="")
+
+#Posicionar els widgets a la finestra
+num_aerogeneradores_label.grid(row=0, column=0)
+num_aerogeneradores_entry.grid(row=0, column=1)
+diametre_label.grid(row=1, column=0)
+diametre_entry.grid(row=1, column=1)
+rendimiento_aerog_label.grid(row=2, column=0)
+rendimiento_aerog_entry.grid(row=2, column=1)
+velocidad_viento_label.grid(row=3, column=0)
+velocidad_viento_entry.grid(row=3, column=1)
+dias_funcionamiento_label.grid(row=4, column=0)
+dias_funcionamiento_entry.grid(row=4, column=1)
+horas_diarias_label.grid(row=5, column=0)
+horas_diarias_entry.grid(row=5, column=1)
+densidad_aire_label.grid(row=6, column=0)
+densidad_aire_entry.grid(row=6, column=1)
+coef_aprovechamiento_viento_label.grid(row=7, column=0)
+coef_aprovechamiento_viento_entry.grid(row=7, column=1)
 precio_venta_label.grid(row=8, column=0)
 precio_venta_entry.grid(row=8, column=1)
-calcular_button.grid(row=9, column=1)
-resultado_label.grid(row=10, column=1)
-beneficio_label.grid(row=11, column=1)
-#Mostrar la finestra
+
+calcular_button.grid(row=9, column=0, columnspan=2)
+
+resultado_label.grid(row=10, column=0, columnspan=2)
+beneficio_label.grid(row=11, column=0, columnspan=2)
+
+#Iniciar la finestra
 window.mainloop()
